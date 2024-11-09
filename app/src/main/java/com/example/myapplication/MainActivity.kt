d734f8a5e6e5d68f7f0b9901f34781e7e599861d
AhmedElazony <ahmedazony2@gmail.com>
2024-11-09 19:54:39 +0200

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.unity3d.player.UnityPlayerActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onUnityButtonClick = {
+                            // Open UnityPlayerActivity when the button is clicked
+                            val intent = Intent(this, UnityPlayerActivity::class.java)
+                            startActivity(intent)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier, onUnityButtonClick: () -> Unit) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Button(
+            onClick = onUnityButtonClick,
+
+        ) {
+            Text(text = "Open Unity Content")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MyApplicationTheme {
+        MainScreen(onUnityButtonClick = {})
+    }
+}
